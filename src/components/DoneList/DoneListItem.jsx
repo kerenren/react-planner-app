69057dedddaf41f7c00497c1b13b47c52881aafd
Kelly <renkelei.kelly@gmail.com>
2020-07-22 00:36:59 +0300

    adjusted toggletotoitem

--- src/components/DoneList/DoneListItem.jsx
+++ src/components/DoneList/DoneListItem.jsx
@@ -13,18 +13,20 @@ function DoneListItem(props) {
   let data = props.doneList;
   data = ["run", "buy wine", "watch movie"];
 
-  function cancelDone(e) {
-    e.preventDefault();
-    e.checked = false;
-    console.log(e);
-    props.cancelDone(e.item);
+  function toggleTodoItem(count, todo) {
+    console.log(count, todo);
+    props.updateTodoItem(count, { todo, finished: false });
   }
 
   let count = 0;
   let li = data.map((taskItem) => {
     return (
-      <List.Item key={count += 1}>
-        <Checkbox checked={true} onChange={(e) => cancelDone(e)} />
+      <List.Item key={(count += 1)}>
+        <Checkbox
+        //   checked={!taskItem.finished}
+          onChange={() => toggleTodoItem(count, taskItem)}
+          defaultChecked={true}
+        />
         {taskItem}
         <EditOutlined
           type="edit"
